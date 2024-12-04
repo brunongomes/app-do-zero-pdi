321dbda0d30ab22e1a4319f9039446333ce125c9
Bruno Gomes <brunongomes8@gmail.com>
2024-12-04 13:44:21 -0300

    feat: add and remove tasks

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,30 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  tarefas = [
+    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
+    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
+    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
+    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
+    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
+    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
+  ]
 
+  addTask() {
+    this.tarefas.push({ titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' });
+  }
+
+  removeTask(index: number) {
+    this.tarefas.splice(index, 1);
+  }
 }
