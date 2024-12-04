a49291a8e26c5cdae9c5de08f2d6ddbed9255dec
Bruno Gomes <brunongomes8@gmail.com>
2024-12-04 13:59:08 -0300

    refactor: fetch task service information

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,5 +1,7 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { TaskServiceService } from '../../services/task-service.service';
+import { Task } from '../../services/task';
 
 @Component({
   selector: 'app-task-list',
@@ -11,20 +13,29 @@ import { Component } from '@angular/core';
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
-  tarefas = [
-    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
-    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
-    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
-    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
-    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
-    { titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' },
-  ]
+  tasks: Task[] = [];
 
-  addTask() {
-    this.tarefas.push({ titulo: 'FINALIZAR ATIVIDADE 1', previsao: '23/11/2024' });
+  constructor(private taskService: TaskServiceService) { }
+
+  ngOnInit():void {
+    this.taskService.getTasks().subscribe(data => this.tasks = data);
   }
 
-  removeTask(index: number) {
-    this.tarefas.splice(index, 1);
+  addTask(): void {
+    const newTask: Task = {
+      title: 'NOVA ATIVIDADE',
+      prevision: '23/11/2024'
+    };
+    this.taskService.addTask(newTask);
+    this.updateTasks();
+  }
+
+  removeTask(index: number): void {
+    this.taskService.removeTask(index);
+    this.updateTasks();
+  }
+
+  private updateTasks(): void {
+    this.taskService.getTasks().subscribe((data) => (this.tasks = data));
   }
 }
