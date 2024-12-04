cf4da5a359aa99602aa014eb2627b17564a1641a
Bruno Gomes <brunongomes8@gmail.com>
2024-12-04 14:33:52 -0300

    feat: add task list service

---
+++ src/app/services/task-list.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from './task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskListService {
+  tasks = [
+    { title: 'FINALIZAR ATIVIDADE 1', prevision: '23/11/2024' },
+    { title: 'FINALIZAR ATIVIDADE 1', prevision: '23/11/2024' },
+    { title: 'FINALIZAR ATIVIDADE 1', prevision: '23/11/2024' },
+    { title: 'FINALIZAR ATIVIDADE 1', prevision: '23/11/2024' },
+    { title: 'FINALIZAR ATIVIDADE 1', prevision: '23/11/2024' },
+    { title: 'FINALIZAR ATIVIDADE 1', prevision: '23/11/2024' },
+  ]
+
+  constructor() { }
+
+  getTasks(): Observable<Task[]> {
+    return of(this.tasks);
+  }
+
+  addTask(task: Task): void {
+    this.tasks.push(task);
+  }
+
+  removeTask(index: number): void {
+    this.tasks.splice(index, 1);
+  }
+}
