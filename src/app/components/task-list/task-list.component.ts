067c39406dd9a93fff3dc4f157f829bb49fda187
Bruno Gomes <brunongomes8@gmail.com>
2024-12-11 16:20:38 -0300

    feat: implement Material Angular table for data display

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { TaskListService } from '../../services/task-list.service';
 import { Task } from '../../services/task';
 import { MatIconModule } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-task-list',
@@ -11,18 +12,24 @@ import { MatIconModule } from '@angular/material/icon';
     NgFor,
     NgClass,
     NgIf,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  displayedColumns: string[] = ['isChecked', 'id', 'title', 'prevision', 'delete'];
   tasks: Task[] = [];
+  dataSource: Task[] = [];
 
   constructor(private taskService: TaskListService) { }
 
   ngOnInit():void {
-    this.taskService.getTasks().subscribe(data => this.tasks = data);
+    this.taskService.getTasks().subscribe(data => {
+      this.tasks = data
+      this.dataSource = data;
+    });
   }
 
   addTask() {
@@ -45,7 +52,10 @@ export class TaskListComponent {
   }
 
   private updateTasks(): void {
-    this.taskService.getTasks().subscribe((data) => (this.tasks = data));
+    this.taskService.getTasks().subscribe((data) => {
+      this.tasks = data;
+      this.dataSource = [...this.tasks];
+    });
   }
 
   onCheckboxChange(index: number) {
